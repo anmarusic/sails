a8a1dfab30f507bcd91c529ef7a2a9adbcf778f5
anmarusic <toni_marusic@hotmail.com>
2017-10-10 11:54:08 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# filmovi
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/FilmoviController.js
@@ -0,0 +1,11 @@
+/**
+ * FilmoviController
+ *
+ * @description :: Server-side logic for managing filmovis
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
